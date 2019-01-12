a80eb1ab8fc022e96395d9794b5ae7e2502ba53f
eric lopez <ericlz.25412@gmail.com>
2019-01-12 15:17:16 -0800

    new

---
+++ triv.js
@@ -0,0 +1,110 @@
+var panel = $('#quiz-area');
+
+
+$(document).on('click', '#start', function(e) {
+  game.start();
+});
+
+$(document).on('click', '#done', function(e) {
+  game.done();
+});
+
+
+var questions = [{
+    question: "When was Apple founded?",
+    answers: ["1999", "2019", "1976", "1987"],
+    correctAnswer: "1976"
+}, 
+{
+    question: "Who owned Apple?",
+    answers: ["Steve jobs", "biill gates", "jeff bezos", "Alien"],
+    correctAnswer: "Steve jobs"
+},
+{
+    question: "Who is Apple's biggest competitor?",
+    answers: ["Samsung", "Microsoft", "Ford", "Windows"],
+    correctAnswer: "Samsung"
+},
+{
+    question: "What is Apple's best selling product?",
+    answers: ["apples", "Iphone", "Airpods", "Apple watch"],
+    correctAnswer: "Iphone"
+}];
+
+var game = {
+  correct:0,
+  incorrect:0,
+  counter:45,
+  countdown: function(){
+    game.counter--;
+    $('#counter-number').html(game.counter);
+
+    if (game.counter === 0){
+      console.log('TIME UP');
+      game.done();
+    }
+  },
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">45</span> Seconds</h2>');
+    $('#start').remove();
+
+
+    for (var i = 0; i < questions.length; i++) {
+      panel.append('<h2>' + questions[i].question + '</h2>');
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append('<input type="radio" name="question' + '-' + i + '" value="' + questions[i].answers[j] + '">' + questions[i].answers[j]);
+      }
+    }
+
+    panel.append('<button id="done">Done</button>');
+  },
+  done: function() {
+
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() == questions[0].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-1']:checked"), function() {
+        if ($(this).val() == questions[1].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() == questions[2].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() == questions[3].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    
+    
+
+    this.result();
+  },
+    result: function() {
+
+    clearInterval(timer);
+
+    $('#subwrapper h2').remove();
+    panel.html('<h2>All Done!</h2>');
+    panel.append('<h3>Correct Answers: ' + this.correct + '</h3>');
+    panel.append('<h3>Incorrect Answers: ' + this.incorrect + '</h3>');
+    panel.append('<h3>Unanswered: ' + (questions.length - (this.incorrect + this.correct)) + '</h3>');
+  }
+
+};
